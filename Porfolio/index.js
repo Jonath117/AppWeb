5b1c87ed2cebf63020a0397119ee642ad3f88fe6
jonathan <rochacontrerasjonathan40@gmail.com>
2025-05-23 12:01:21 -0400

    router

--- Porfolio/index.js
+++ Porfolio/index.js
@@ -1,5 +1,13 @@
+import { Router } from "./services/Router.js";
 import { initBlogEvents } from './services/blogService.js';
 
 initBlogEvents(
     document.querySelector('.blog')
 );
+
+globalThis.app = {};
+app.router = Router;
+
+window.addEventListener("DOMContentLoaded", () => {
+  app.router.init();
+});
